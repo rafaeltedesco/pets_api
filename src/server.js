b8a8e8b7a5ae71ba292a65347deec9255a5ef229
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-03-22 10:04:19 -0300

    style: fix lint

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const logger = require('./utils/logger');
 const app = require('./app');
 
-const PORT = process.env.NODE_ENV === 'production'? 5001 : process.env.API_PORT || 3000;
+const PORT = process.env.NODE_ENV === 'production' ? 5001 : process.env.API_PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`Up and running on PORT ${PORT} 🐶`);
