c198e38c0f5f5d586f12dd0bb5569b9e7d352ac0
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2023-04-13 19:39:08 -0300

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
+const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-});
+}
+
+console.log(sqlConfig);
+
+const connection = mysql.createPool(sqlConfig);
 
 module.exports = connection;
