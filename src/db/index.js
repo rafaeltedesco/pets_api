6914d10128ea3fd58aec3836bbcac70702e9b38e
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2023-04-13 19:48:26 -0300

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,7 @@ const sqlConfig = {
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 }
 
 console.log(sqlConfig);
